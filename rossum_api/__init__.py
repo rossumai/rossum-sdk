a070a6093ee4633ca47053058e033c7d5f17422a
semantic-release <semantic-release>
2025-09-10 12:31:06 +0000

    3.1.3
    Automatically generated by python-semantic-release

--- rossum_api/__init__.py
+++ rossum_api/__init__.py
@@ -4,7 +4,7 @@ from rossum_api.clients.external_async_client import AsyncRossumAPIClient
 from rossum_api.clients.external_sync_client import SyncRossumAPIClient
 from rossum_api.exceptions import APIClientError
 
-__version__ = "3.1.2"
+__version__ = "3.1.3"
 
 __all__ = (
     "AsyncRossumAPIClient",
